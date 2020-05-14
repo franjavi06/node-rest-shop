1c29761ede53c46d84fc08be381b02915a705b62
FranJ <franjavi06@gmail.com>
2020-05-14 12:24:44 -0400

    firstcommit

---
+++ api/routes/orders.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
+
+const OrdersController = require('../controllers/orders');
+
+router.get('/', checkAuth, OrdersController.orders_get_all);
+
+router.post('/', checkAuth, OrdersController.orders_create_order);
+
+router.get('/:orderId', checkAuth, OrdersController.orders_get_order);
+
+router.delete('/:orderId', checkAuth, OrdersController.orders_delete_order);
+
+/*router.patch('/:orderId', checkAuth, (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated Order'
+    });
+});*/
+
+module.exports = router;
